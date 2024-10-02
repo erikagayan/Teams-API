2f39d9c19bba9872e236601a8f74b9ec55635505
Erik <erik.agayan1@gmail.com>
2024-10-02 16:08:15 +0300

    Get username insted id in  teams

--- teams/serializers.py
+++ teams/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 from teams.models import Team
-from users.serializers import UsernameSerializer
+from users.models import User
 
 
 class TeamSerializer(serializers.ModelSerializer):
-    members = UsernameSerializer(many=True, read_only=True)
+    members = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=User.objects.all(),
+        write_only=True
+    )
+    members_info = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Team
-        fields = ["id", "name", "members"]
+        fields = ['id', 'name', 'members', 'members_info']
+
+    def get_members_info(self, obj):
+        return [user.username for user in obj.members.all()]
