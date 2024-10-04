4d11bc3ac82ecdaf543ddf6e098ff94d22f3b707
Erik <erik.agayan1@gmail.com>
2024-10-04 13:45:49 +0300

    Add postgresql

--- teamsAPI/settings.py
+++ teamsAPI/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = "teamsAPI.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "teams",
+        "USER": "admin",
+        "PASSWORD": "admin",
+	    "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
